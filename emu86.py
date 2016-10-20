e8571cb1c1f020b674ee04a00d8640308bf4ac6a
Tyler Johnson <jtylers@byu.edu>
2016-10-20 17:46:25 -0600

    Added better command line functionality

--- emu86.py
+++ emu86.py
@@ -1,11 +1,14 @@
 #!/bin/python
 import os
 import sys
-sys.path.insert(0,'pexpect-4.2.1/pexpect')
 import pexpect
+import time
+
 # emu86.py
 # Runs emu86 and passes it the arguments to this script one at a time
 # Single letter arguments are passed with the next argument if it is longer than 1 character
+# passing a "B" "C" or "T" will be turned into ctrl + that letter in lowercase
+# passing a "W" and then a number will delay for that many milliseconds
 
 commands = ([])
 i = 1
@@ -23,7 +26,12 @@ emu = pexpect.spawn('emu86')
 
 # Run all the commands in order
 for command in commands:
-    emu.sendline(command)
+    if  (command == "B "): emu.sendcontrol('b')
+    elif(command == "C "): emu.sendcontrol('c')
+    elif(command == "T "): emu.sendcontrol('t')
+    elif("W " in command): 
+        time.sleep(float(command.split()[1]) / 1000.0)
+    else: emu.sendline(command)
 
 # Pass interaction to the user
 emu.interact()
